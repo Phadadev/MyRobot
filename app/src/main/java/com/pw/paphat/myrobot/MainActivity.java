d8a10c652b03f5f345b5da7ccdb1abd99f29d48c
NYR <paphat.dev@gmail.com>
2016-10-08 15:00:35 +0700

    finish 8 Oct 2016

--- app/src/main/java/com/pw/paphat/myrobot/MainActivity.java
+++ app/src/main/java/com/pw/paphat/myrobot/MainActivity.java
@@ -1,14 +1,16 @@
 package com.pw.paphat.myrobot;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     //Explicit
-    private TextView textView;
+    private TextView textView,textView2;
     private SeekBar seekBar;
     private int anInt = 0;
 
@@ -19,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Bind Widget
         textView = (TextView) findViewById(R.id.textView);
+//        textView2 = (TextView) findViewById(R.id.textView2);
         seekBar = (SeekBar) findViewById(R.id.seekBar);
 
         //SeekBar controller
@@ -26,9 +29,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {//when change will want to do?
 
-
+                seekBar.setMax(1000);
                 anInt = progress;
                 textView.setText(Integer.toString(anInt)); // change interger to string
+
+
+
+                //uploadIntegertoDweet(anInt);
 
 
             }// onProgress
@@ -41,9 +48,40 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
 
+                //textView2.setText(Integer.toString(anInt));
+                Log.d("RobotVI", "This is my message"+ anInt);
+                uploadIntegertoDweet(anInt);
+
             }
         });
 
 
     }   //Main Method
+
+    private void uploadIntegertoDweet(int anInt) {
+
+
+
+    }// upload
+
+    private class UpLoadValue extends AsyncTask<Integer, Void, String> {
+
+
+        @Override
+        protected String doInBackground(Integer... params) {
+
+            return null;
+        }// doInBackground
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+
+        }   //onPost
+
+
+
+    } // UpLoadValue
+
 }   //Main Class
